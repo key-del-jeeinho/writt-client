0077a8673f9401aff9f965c869d8752be1f324d8
jeeinho <inho@enhans.ai>
2024-10-05 20:08:33 +0900

    fix: editor z-index (for to be positioned above the editor when scrolling)

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 export default function TopBar({ activeTab, deactiveTabs }: IProps) {
     return (
-        <div className="sticky top-0 self-start w-full">
+        <div className="sticky top-0 self-start w-full z-10">
           <div className="h-12 w-full bg-zinc-900 flex flex-row gap-1 items-end p-0">
             <EditorTab activeTab={activeTab} deactiveTabs={deactiveTabs} />
           </div>
